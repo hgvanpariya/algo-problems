d74ff1922e49da05f5e006f5cb32f684016d0bdf
hgvanpariya <h.g.vanpariya@gmail.com>
2017-05-24 22:00:39 +0530

    Singleton design pattern

---
+++ algo-problems/src/code/concept/SingletonDesignPattern.java
@@ -0,0 +1,41 @@
+package code.concept;
+
+/**
+ * This is singleton design pattern which will help to create the object of the
+ * class only once.
+ * 
+ * @author harikrushna
+ *
+ */
+public class SingletonDesignPattern {
+
+	private static SingletonDesignPattern singletonDesignPattern;
+
+	/**
+	 * Private construction with the exception to avoid creating the object
+	 * using Java Reflection .
+	 */
+	private SingletonDesignPattern() {
+		if (singletonDesignPattern != null) {
+			throw new IllegalStateException("Unable to create already created object.");
+		}
+	}
+
+	/**
+	 * Get the object of the singleton instance.
+	 * 
+	 * @return object of {@link SingletonDesignPattern}.
+	 */
+	public SingletonDesignPattern getInstance() {
+		if (singletonDesignPattern == null) {
+			synchronized (SingletonDesignPattern.class) {
+				if (singletonDesignPattern == null) {
+					singletonDesignPattern = new SingletonDesignPattern();
+				}
+
+			}
+		}
+
+		return singletonDesignPattern;
+	}
+}
